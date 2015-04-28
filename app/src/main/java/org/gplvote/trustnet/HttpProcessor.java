6759b08d7e8739994d84899423cf6aebc873ace8
Andrey Velikoredchanin <andy@andyhost.ru>
2015-04-28 15:00:04 +0300

    Base class for HTTP operations

--- app/src/main/java/org/gplvote/trustnet/HttpProcessor.java
+++ app/src/main/java/org/gplvote/trustnet/HttpProcessor.java
@@ -1,7 +1,97 @@
 package org.gplvote.trustnet;
 
-import org.apache.http.HttpResponse;
+import android.util.Log;
 
-public interface HttpProcessor {
-    public boolean onResponse(int id, HttpResponse response);
+import com.google.gson.Gson;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class HttpProcessor {
+    private HttpClient httpclient;
+    public JsonResponse json_response;
+
+    public HttpProcessor() {
+        httpclient = new DefaultHttpClient();
+    }
+
+    public boolean postData(String url, String json) {
+        Log.d("HTTP postData", "HTTP deliver doc to "+url+": " + json);
+        HttpPost httppost = new HttpPost(url);
+
+        try {
+            // Add your data
+            StringEntity se = null;
+            se = new StringEntity(json);
+            httppost.setEntity(se);
+            httppost.setHeader("Accept", "application/json");
+            httppost.setHeader("Content-type", "application/json");
+
+            // Execute HTTP Post Request
+            HttpResponse response = httpclient.execute(httppost);
+
+            String body = EntityUtils.toString(response.getEntity(), "UTF-8");
+            int http_status = response.getStatusLine().getStatusCode();
+            if (http_status == 200) {
+                Log.d("HTTP postData", "HTTP response 200 with body: " + body);
+                Gson gson = new Gson();
+                json_response = gson.fromJson(body, JsonResponse.class);
+                return (json_response != null && json_response.status == 200);
+            } else {
+                Log.e("HTTP postData", "HTTP response: "+http_status+" body: "+body);
+                return false;
+            }
+        } catch (UnsupportedEncodingException e) {
+            Log.e("HTTP postData", "Error HTTP request: ", e);
+            return false;
+        } catch (ClientProtocolException e) {
+            Log.e("HTTP postData", "Error HTTP request: ", e);
+            return false;
+        } catch (IOException e) {
+            Log.e("HTTP postData", "Error HTTP request: ", e);
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public String getData(String url) {
+        Log.d("HTTP getData", "URL = "+url);
+
+        HttpGet httpget = null;
+        try {
+            httpget = new HttpGet(new URI(url));
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            HttpResponse response = httpclient.execute(httpget);
+
+            String body = EntityUtils.toString(response.getEntity(), "UTF-8");
+
+            Log.d("HTTP getData", "Answer body = "+body);
+            int http_status = response.getStatusLine().getStatusCode();
+            if (http_status == 200) {
+                Log.d("HTTP getData", "Status 200");
+                return body;
+            } else {
+                Log.e("AsyncHTTP", "Error HTTP response status " + http_status);
+                return nil;
+            }
+        } catch (Exception e) {
+            Log.e("AsyncHTTP", "Error HTTP request: ", e);
+            return nil;
+        }
+    }
 }
---
+++ app/src/main/java/org/gplvote/trustnet/JsonResponse.java
@@ -0,0 +1,8 @@
+package org.gplvote.trustnet;
+
+public class JsonResponse {
+    public int status;
+    public String error;
+    public Long time;
+    public String doc;
+}
